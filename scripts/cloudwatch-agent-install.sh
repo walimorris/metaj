7891d385d126a6289283ef4d702a125c13af0db2
Wali Morris <walimmorris@gmail.com>
2022-11-27 00:22:05 -0800

    create ServiceTestConfiguration

---
+++ scripts/cloudwatch-agent-install.sh
@@ -0,0 +1,2 @@
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
---
+++ src/test/java/com/morris/metaj/ServiceTestConfiguration.java
@@ -0,0 +1,67 @@
+package com.morris.metaj;
+
+import com.morris.metaj.service.impl.*;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class ServiceTestConfiguration {
+
+    @Bean
+    @Primary
+    public CloudWatchServiceImpl cloudWatchService() {
+        return Mockito.mock(CloudWatchServiceImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public MetaJS3ServiceImpl metaJS3Service() {
+        return Mockito.mock(MetaJS3ServiceImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public AwsClientsBuilderImpl awsClientsBuilder() {
+        return Mockito.mock(AwsClientsBuilderImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public CommandExecutorImpl commandExecutor() {
+        return Mockito.mock(CommandExecutorImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public InstanceCommandsImpl instanceCommands() {
+        return Mockito.mock(InstanceCommandsImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public InstanceInitializerImpl instanceInitializer() {
+        return Mockito.mock(InstanceInitializerImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public InstanceMappingsImpl instanceMappings() {
+        return Mockito.mock(InstanceMappingsImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public MetaJAwsIamServiceImpl metaJAwsIamService() {
+        return Mockito.mock(MetaJAwsIamServiceImpl.class);
+    }
+
+    @Bean
+    @Primary
+    public MetaJSetupServiceImpl metaJSetupService() {
+        return Mockito.mock(MetaJSetupServiceImpl.class);
+    }
+}
